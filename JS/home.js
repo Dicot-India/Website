6287e6d03ad2e262e2f756edc711c5a0c306494c
imdiode <nandanparagshukla@gmail.com>
2021-03-23 10:45:42 +0530

    meta and favicon

--- JS/home.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $("#b1").on('click', function(){
-        $("#s1").fadeOut();
-        $("#s1").removeClass(' vis');
-        $("#s1").addClass(' vis');
-        $("#s2").delay(3000).fadeIn();
-    });
-});
