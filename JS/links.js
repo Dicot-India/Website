4d93181ee0c2fed5c23d72290241998770ab8136
Nandan Shukla <nandanparagshukla@gmail.com>
2021-04-05 22:14:47 +0530

    links
    updated social media links, completed links.js file

---
+++ JS/links.js
@@ -0,0 +1,43 @@
+function to_home() {
+    location.href = "home.html";
+}
+
+function to_about() {
+    location.href = "about.html";
+}
+
+function to_vc() {
+    location.href = "VisionC.html";
+}
+
+function to_vg() {
+    location.href = "VGraphic.html";
+}
+
+function to_vw() {
+    location.href = "coming.html";
+}
+
+function to_contact() {
+    location.href = "contact.html";
+}
+
+function to_vwlogin() {
+    location.href = "about.html";
+}
+
+function to_linkedin() {
+    location.href = "https://www.linkedin.com/company/dicot-india";
+}
+
+function to_facebook() {
+    location.href = "https://www.facebook.com/tech.dicot";
+}
+
+function to_instagram() {
+    location.href = "https://www.instagram.com/dicot.india";
+}
+
+function to_twitter() {
+    location.href = "https://www.twitter.com/DicotIndia";
+}
