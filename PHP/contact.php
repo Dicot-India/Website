e78aae2e67197945903a51076f04940847e2a304
imdiode <nandanparagshukla@gmail.com>
2021-04-07 10:01:52 +0530

    contact i#1

--- PHP/contact.php
+++ PHP/contact.php
@@ -1,3 +1,4 @@
+<?php 
 require('recaptcha-master/src/autoload.php');
 
 $recaptchaSecret = '6LfPhJsaAAAAAF2Xzi4FPyS7cUXZZ1JA0aIjJCBF';
@@ -10,5 +11,19 @@ if (!$response->isSuccess()) {
 
 }
 else {
+    if(isset($_POST['submit'])){
+        $to = "sales@dicot.in"; // this is your Email address
+        $from = $_POST['email']; // this is the sender's Email address
+        $first_name = $_POST['name'];
+        $subject = "Inquiry";
+        $message = $first_name . " contact no: " . $_POST['tel'] . " Email: " . $from . " wrote the following:" . "\n\n" . $_POST['text-ans'];
+        $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['text-ans'];
     
+        $headers = "From:" . $from;
+        mail($to,$subject,$message,$headers);
+        echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+        // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
 }
+
+?>
