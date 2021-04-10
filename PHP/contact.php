7b5a1627afa352aea6ab668956ca501471683976
imdiode <nandanparagshukla@gmail.com>
2021-04-10 18:48:34 +0530

    contact.php working

--- PHP/contact.php
+++ PHP/contact.php
@@ -1,50 +1,41 @@
 <?php 
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
-
     require 'src/Exception.php';
     require 'src/PHPMailer.php';
     require 'src/SMTP.php';
+    
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
 
     $mail = new PHPMailer(true);
     $mail->IsSMTP();
     $mail->Mailer = "smtp";
 
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                             //Enable SMTP authentication
-    $mail->Username   = 'dicotindia@gmail.com';                     //SMTP username
-    $mail->Password   = 'naseeb06012020';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 465;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.gmail.com';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'dicotindia@gmail.com';                     
+    $mail->Password   = 'naseeb06012020';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->Port       = 465;
 
 
-    $to = "sales@dicot.in"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
+    $to = "sales@dicot.in";
+    $from = $_POST['email'];
     $first_name = $_POST['name'];
-<<<<<<< HEAD
-    $subject = "Inquiry";
-    $message = "Message:" . "\n\n" . $_POST['textans'];
-    $mail->setFrom($from, $first_name);
-    $mail->addAddress($to, 'Sales Team');
-    $mail->Subject = $subject;
-    $mail->AltBody=$message;
-    $mail->send();
-    //You can change 'Sent Mail' to any other folder or tag
-    $path = '{imap.gmail.com:993/imap/ssl}[Gmail]/Sent Mail';
 
-    //Tell your server to open an IMAP connection using the same username and password as you used for SMTP
-    $imapStream = imap_open($path, $mail->Username, $mail->Password);
-
-    $result = imap_append($imapStream, $path, $mail->getSentMIMEMessage());
-    imap_close($imapStream);
-=======
     $subject = "Inquiry Dicot";
-    $message = $first_name . " contact no: " . $_POST['tel'] . " Email: " . $from . " wrote the following:" . "\n\n" . $_POST['textans'];
-
+    $message = "Message: " . $_POST['textans'];
     $headers = "From:" . $from;
 
-    email($to,$subject,$message,$headers);
->>>>>>> 29dfee27e885dff3d923e456c11918249c0dbfce
+/*     email($to,$subject,$message,$headers); */
+    $mail->setFrom($from, $first_name);
+    $mail->addAddress('sales@dicot.in', 'Sales Team');
+    $mail->Subject = $subject;
+    $mail->AltBody=$message;
+    if(!$mail->send())
+    {
+        echo "Didn't send mail!";
+    }
+    
 ?>
