102103c0d14613ef9f9e9e37affbe644473fc3d5
Nandan Shukla <nandanparagshukla@gmail.com>
2021-04-15 14:20:27 +0530

    adding jquery

--- PHP/contact.php
+++ PHP/contact.php
@@ -11,16 +11,21 @@
     $mail = new PHPMailer(true);
     $mail->IsSMTP();
     $mail->Mailer = "smtp";
-
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-    $mail->isSMTP();
+    
+    $mail->SMTPDebug = 3;//SMTP::DEBUG_SERVER;
     $mail->Host       = 'smtp.gmail.com';
-    $mail->SMTPAuth   = true;
+    $mail->SMTPAuth = true;
     $mail->Username   = 'dicotindia@gmail.com';                     
     $mail->Password   = 'naseeb06012020';
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-    $mail->Port       = 465;
-
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port       = 587;
+    $mail->SMTPOptions = array(
+	'ssl'=> array(
+	    'verify_peer' => false,
+	    'verify_peer_name' => false,
+	    'allow_self_signed' => true,
+	)
+    );
 
     $to = "sales@dicot.in";
     $from = $_POST['email'];
@@ -28,10 +33,9 @@
 
     $subject = "Inquiry Dicot from " . $first_name;
     $message = " Name: ". $first_name . " Email: " . $from . " Contact No.: " . $_POST['tel'] . " Message: " . $_POST['textans'];
-    $headers = "From:" . $from;
-
-/*     email($to,$subject,$message,$headers); */
-    $mail->SetFrom($from, $first_name);
+    
+    //email($to,$subject,$message,$headers);
+    $mail->SetFrom($from, 'Contact form');
     $mail->addAddress('sales@dicot.in', 'Sales Team');
     $mail->Subject = $subject;
     $mail->Body=$message;
